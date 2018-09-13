7c4cda1a42b3e8f78072eab0dacb925d1dc71814
Gautam Ramaswamy <grr37@cornell.edu>
2018-09-13 08:27:40 -0700

    Make tray only show up on first monitor

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,10 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+tray_pos="right"
+
 # Launch bar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1 | sort); do
-    MONITOR=$m polybar --reload main &
+    MONITOR=$m TRAY_POS=$tray_pos polybar --reload main &
+    tray_pos=""
   done
 else
   polybar --reload main &
